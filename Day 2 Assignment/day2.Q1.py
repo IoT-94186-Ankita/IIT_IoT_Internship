58600a70eeee81be67ae996ae0e40f3cfcd08fcc
ankita <ankitawante08@gmail.com>
2025-12-13 11:12:19 +0530

    Day 2 Assignment push

---
+++ Day 2 Assignment/day2.Q1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number (dividend): "))
+num2 = float(input("Enter the second number (divisor): "))
+
+if num2 == 0:
+    print("Error: Division by zero is not allowed.")
+else:
+    result = num1 / num2
+    print(f"Division result is={result}")
